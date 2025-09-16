a46bbb1d112e556683698cdf9b7a1b03a13363c3
Christian <97773844+esuriru@users.noreply.github.com>
2025-09-17 00:23:33 +0800

    Add test code for calling functions

--- examples/simple_console/native/main.cpp
+++ examples/simple_console/native/main.cpp
@@ -1,11 +1,19 @@
 #include "bind.hpp"
+#include "dotnet/assembly.hpp"
 
 int main()
 {
-    bind::core::app app =
-        bind::core::app()
-            .run_hostfxr("")
-            .load_assembly("");
+    bind::dotnet::assembly* assembly = nullptr;
+
+    bind::core::app app;
+
+    app.run_hostfxr("Debug/net9.0/managed.runtimeconfig.json")
+        .load_assembly("Debug/net9.0/managed.dll", assembly);
+
+    if (assembly)
+    {
+        assembly->call_func(L"SimpleConsole.Test", L"Write");
+    }
 
     return 0;
 }
