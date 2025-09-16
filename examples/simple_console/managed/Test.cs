ca15d3291f53b595b21a92eaeae5b8e01a8a9350
Christian <97773844+esuriru@users.noreply.github.com>
2025-09-17 00:22:01 +0800

    Add test C# program

---
+++ examples/simple_console/managed/Test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace SimpleConsole 
+{
+    public static class Test 
+    {
+        [UnmanagedCallersOnly]
+        public static void Write()
+        {
+            Console.WriteLine("Called from Native");
+        }
+    }
+}
